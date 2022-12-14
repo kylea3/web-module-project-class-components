36c47db93113d463c8ff959eaaec99c6fd9eb6af
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-14 15:26:52 -0700

    start Todo list component inside App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,22 +1,42 @@
 import React from 'react';
 
 
+
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [{
+          task: 'Bake Cookies',
+          id: 12124234234,
+          completed: false
+        },
+      {
+        task: 'Eat Cookies',
+      id: 12323123234,
+      completed: false
+    }]
+    }
+  }
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
         <h1>Todo</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Walk the cat</li>
-          <li>Walk the fish</li>
+          {todos.map(el => {
+            return (
+              <li>{el.task}{el.completed ? ' ✔' : ''}</li>
+          )}
+          )}
         </ul>
 
         <form>
           <input />
           <button>Add</button>
         </form>
-        
+
         <button>Clear</button>
       </div>
     );
