d43eaaf1cf3c7e6adb1569e49df116331c315bc4
Kyle Andrus <kyleandrus3@gmail.com>
2022-12-14 15:09:23 -0700

    add in all structure

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Walk the cat</li>
+          <li>Walk the fish</li>
+        </ul>
+
+        <form>
+          <input />
+          <button>Add</button>
+        </form>
+        
+        <button>Clear</button>
       </div>
-    )
+    );
   }
 }
+
